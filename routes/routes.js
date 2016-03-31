494353f2f6c477d137e796e2131200a4c9307222
Emma J <codemzy@gmail.com>
2016-03-31 15:24:37 +0000

    Added routes.js

---
+++ routes/routes.js
@@ -0,0 +1,73 @@
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended: true });
+
+require('dotenv').config();
+
+module.exports = function (app, db, passport) {
+    
+    // function to check if user is logged in
+	function isLoggedIn (req, res, next) {
+		if (req.isAuthenticated()) {
+			return next();
+		} else {
+			res.redirect('/');
+		}
+	}
+    
+    // direct user to correct app if logged in or anon
+    app.route('/')
+        .get(function (request, response) {
+    		if (request.isAuthenticated()) {
+    			response.sendFile(process.cwd() + '/public/app/loggedin.html');
+    		} else {
+    			response.sendFile(process.cwd() + '/public/app/index.html');
+    		}
+            
+        });
+        
+    // ANON APIS
+
+        
+    // LOGGED IN APIS
+    // get user data
+    app.route('/api/user')
+        .get(isLoggedIn, function(req, res) {
+			res.json(req.user);
+        });
+    
+
+        
+    // authentication routes (FIRST LOG IN)
+        
+	app.route('/auth/twitter')
+		.get(passport.authenticate('twitter'));
+
+	app.route('/auth/twitter/callback')
+		.get(passport.authenticate('twitter', {
+			successRedirect: '/',
+			failureRedirect: '/'
+		}));
+		
+	app.route('/auth/facebook')
+	    .get(passport.authenticate('facebook'));
+	    
+	app.route('/auth/facebook/callback')
+		.get(passport.authenticate('facebook', {
+			successRedirect: '/',
+			failureRedirect: '/'
+		}));
+		
+	
+	// authorize routes (CONNECT ADDITIONAL ACCOUNT)
+	// put the logic in the above authentication routes, in future could have seperate strategy as per passport docs
+
+	
+	// LOG OUT
+		
+	app.route('/logout')
+		.get(function (req, res) {
+			req.logout();
+			res.redirect('/');
+		});
+		
+};
