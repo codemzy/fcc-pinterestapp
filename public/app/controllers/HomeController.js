9b2edc79be310677bc76f8607f2aca536bae5d29
Emma J <codemzy@gmail.com>
2016-04-04 13:30:02 +0000

    Added masonry directive and got grid working on home page

--- public/app/controllers/HomeController.js
+++ public/app/controllers/HomeController.js
@@ -1,21 +1,27 @@
 angular.module('FinterestApp')
 .controller('HomeController', ['$scope', function($scope) {
-    $('.grid').masonry({
-      // options
-      itemSelector: '.grid-item',
-      columnWidth: 200
-    });
     $scope.items = [
-            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_IMG_9542.jpg" },
-            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_IMG_9542.jpg" },
-            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_IMG_9542.jpg" },
-            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_IMG_9542.jpg" },
-            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_IMG_9542.jpg" },
-            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_IMG_9542.jpg" }
+            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_DSC_3274.jpg", name: "Whateves" },
+            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_IMG_0499.jpg", name: "Whateves" },
+            { url: "http://autopilotonlinesuccess.com/wp-content/uploads/2009/11/royalty-free-multimedia.jpg", name: "Whateves" },
+            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_IMG_1166.jpg", name: "Whateves with a much longer title than before" },
+            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_IMG_1275.jpg" },
+            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_IMG_9998.jpg" },
+            { url: "http://autopilotonlinesuccess.com/wp-content/uploads/2009/11/royalty-free-multimedia.jpg" }
         ];
+    $scope.titleShow = false;
+    /*
     for (var i = 0; i < $scope.items.length; i++) {
           var $content = $( '<div class="grid-item"><img class="responsive-img" src="' + $scope.items[i].url + '" alt="" /></div>' );
           // add jQuery object
           $('.grid').append( $content ).masonry( 'appended', $content );
+          if (i == $scope.items.length-1) {
+            $('.grid').masonry({
+              // options
+              itemSelector: '.grid-item',
+              columnWidth: 200
+            });
+          }
     }
+    */
 }]);
