8954e27691d9c187bc3cb193a5e4e667d5927d18
Emma J <codemzy@gmail.com>
2016-04-05 16:29:27 +0000

    Tested add img routes and updated design

--- public/app/controllers/User_ImgAddController.js
+++ public/app/controllers/User_ImgAddController.js
@@ -1,8 +1,8 @@
 angular.module('FinterestApp')
 .controller('User_ImgAddController', ['$scope', 'imgs', function($scope, imgs) {
     $scope.formData = {};  
-    $scope.addImage = function () {
-      if ($scope.formData.title.match(/[^a-z\s0-9?]/ig)) {
+    $scope.addImage = function() {
+      if ($scope.formData.imgTitle.match(/[^a-z\s0-9?]/ig)) {
         $scope.helpForm = 'Invalid Information. Please remove punctuation (valid characters are letters, numbers, spaces.)';
       } else {
         var imgData = $.param($scope.formData);
