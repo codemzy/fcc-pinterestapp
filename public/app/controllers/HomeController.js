690e9718808222b3a5b87c5ff16642b34630b866
Emma J <codemzy@gmail.com>
2016-04-07 11:59:22 +0000

    Updated home view to use masonry with cards - without directive

--- public/app/controllers/HomeController.js
+++ public/app/controllers/HomeController.js
@@ -2,18 +2,19 @@ angular.module('FinterestApp')
 .controller('HomeController', ['$scope', 'imgs', function($scope, imgs) {
     imgs.getImgs().success(function(data) {
       $scope.items = data;
+      // apply the masonry grid to the view
+      setTimeout(function () {
+          $scope.$apply(function () {
+            angular.element('.grid').masonry({
+              // options
+              itemSelector: '.grid-item',
+              columnWidth: 200
+            });
+          });
+      }, 10);
     });
-    $scope.items = [
-            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_DSC_3274.jpg", name: "Whateves" },
-            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_IMG_0499.jpg", name: "Whateves" },
-            { url: "http://autopilotonlinesuccess.com/wp-content/uploads/2009/11/royalty-free-multimedia.jpg", name: "Whateves" },
-            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_IMG_1166.jpg", name: "Whateves with a much longer title than before" },
-            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_IMG_1275.jpg" },
-            { url: "http://netdna.webdesignerdepot.com/uploads/2013/11/picjumbo.com_IMG_9998.jpg" },
-            { url: "http://autopilotonlinesuccess.com/wp-content/uploads/2009/11/royalty-free-multimedia.jpg" }
-        ];
     $scope.titleShow = false;
-    /*
+    /* REMOVED SO CAN USE NGREPEAT 
     for (var i = 0; i < $scope.items.length; i++) {
           var $content = $( '<div class="grid-item"><img class="responsive-img" src="' + $scope.items[i].url + '" alt="" /></div>' );
           // add jQuery object
@@ -26,5 +27,5 @@ angular.module('FinterestApp')
             });
           }
     }
-    */
+    REMOVED SO CAN USE NGREPEAT  */
 }]);
--- public/app/controllers/User_ImgMyController.js
+++ public/app/controllers/User_ImgMyController.js
@@ -12,21 +12,6 @@ angular.module('FinterestApp')
             });
           });
       }, 10);
-      /* REMOVED SO CAN USE NGREPEAT 
-      for (var i = 0; i < $scope.items.length; i++) {
-            var $content = $( '<div class="grid-item"><img class="responsive-img" src="' + $scope.items[i].img_url + '" alt="" /></div>' );
-            // add jQuery object
-            $('.grid').append( $content ).masonry( 'appended', $content );
-            if (i == $scope.items.length-1) {
-              $('.grid').masonry({
-                // options
-                itemSelector: '.grid-item',
-                columnWidth: 200
-              });
-            }
-      }
-      
-      REMOVED SO CAN USE NGREPEAT */
     });
     $scope.titleShow = false;
 }]);
