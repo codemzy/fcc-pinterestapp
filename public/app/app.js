adb80802e395027fea119ba8dfb4153792ba8a2f
Emma J <codemzy@gmail.com>
2016-04-07 19:39:39 +0000

    Broken image display added

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,25 @@
 angular.module('FinterestApp', ['ngRoute'])
 
+// directive for default img if error
+.directive('img', function () {
+    return {
+        restrict: 'E',        
+        link: function (scope, element, attrs) {     
+            // show an image-missing image
+            element.error(function () {
+                var w = element.width();
+                var h = element.height();
+                // using 20 here because it seems even a missing image will have ~18px width 
+                // after this error function has been called
+                if (w <= 20) { w = 100; }
+                if (h <= 20) { h = 100; }
+                var url = '/public/img/finterest_noimg.jpg';
+                element.prop('src', url);
+            });
+        }
+    };
+})
+
 // app routes
 .config(function($routeProvider, $locationProvider) {
   $routeProvider
--- public/app/app_user.js
+++ public/app/app_user.js
@@ -1,6 +1,29 @@
 
 angular.module('FinterestApp', ['ngRoute'])
 
+// directive for default img if error
+.directive('img', function () {
+    return {
+        restrict: 'E',        
+        link: function (scope, element, attrs) {     
+            // show an image-missing image
+            element.error(function () {
+                var url = '/public/img/finterest_noimg.jpg';
+                element.prop('src', url);
+                setTimeout(function () {
+                    scope.$apply(function () {
+                      angular.element('.grid').masonry({
+                        // options
+                        itemSelector: '.grid-item',
+                        columnWidth: 300
+                      });
+                    });
+                }, 200);
+            });
+        }
+    };
+})
+
 // app routes
 .config(function($routeProvider, $locationProvider) {
   $routeProvider
