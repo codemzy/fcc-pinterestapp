2a1419f39ade635cf766f64dfe32a6583512a4fb
Emma J <codemzy@gmail.com>
2016-04-07 19:54:36 +0000

    Added broken image directive to app.js

--- public/app/app.js
+++ public/app/app.js
@@ -7,14 +7,17 @@ angular.module('FinterestApp', ['ngRoute'])
         link: function (scope, element, attrs) {     
             // show an image-missing image
             element.error(function () {
-                var w = element.width();
-                var h = element.height();
-                // using 20 here because it seems even a missing image will have ~18px width 
-                // after this error function has been called
-                if (w <= 20) { w = 100; }
-                if (h <= 20) { h = 100; }
                 var url = '/public/img/finterest_noimg.jpg';
                 element.prop('src', url);
+                setTimeout(function () {
+                    scope.$apply(function () {
+                      angular.element('.grid').masonry({
+                        // options
+                        itemSelector: '.grid-item',
+                        columnWidth: 300
+                      });
+                    });
+                }, 200);
             });
         }
     };
