361edc4fc6569f819f272c537a8d545cc2b6be12
Emma J <codemzy@gmail.com>
2016-04-07 11:52:04 +0000

    Grid with cards working for my images

--- public/app/controllers/User_ImgMyController.js
+++ public/app/controllers/User_ImgMyController.js
@@ -2,6 +2,17 @@ angular.module('FinterestApp')
 .controller('User_ImgMyController', ['$scope', 'imgs', function($scope, imgs) {
     imgs.getMyImgs().success(function(data) {
       $scope.items = data;
+      // apply the masonry grid to the view
+      setTimeout(function () {
+          $scope.$apply(function () {
+            angular.element('.grid').masonry({
+              // options
+              itemSelector: '.grid-item',
+              columnWidth: 200
+            });
+          });
+      }, 10);
+      /* REMOVED SO CAN USE NGREPEAT 
       for (var i = 0; i < $scope.items.length; i++) {
             var $content = $( '<div class="grid-item"><img class="responsive-img" src="' + $scope.items[i].img_url + '" alt="" /></div>' );
             // add jQuery object
@@ -14,11 +25,8 @@ angular.module('FinterestApp')
               });
             }
       }
+      
+      REMOVED SO CAN USE NGREPEAT */
     });
     $scope.titleShow = false;
-    $('.grid').masonry({
-      // options
-      itemSelector: '.grid-item',
-      columnWidth: 200
-    });
 }]);
